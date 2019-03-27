a7d4c7fc6a1473341b54763ebc1f0eb3522a52d7
persistever <persitever@hotmail.com>
2019-03-27 15:02:13 +0800

    delete some file
    delete some file

--- train.bat
+++ train.bat
@@ -1,4 +1,4 @@
-call activate otcproject
+conda activate ObstacleTower
 D:
 cd D:\Unity\otc
 python -um dopamine.discrete_domains.train --base_dir="." --gin_files=".\\dopamine\\agents\\dqn\\configs\\dqn.gin"
